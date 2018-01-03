bbe8aa8f602afa75d45a5073863758b8f87b8f72
vedabrat <vedabrat>
2018-01-03 18:54:17 -0500

    RetrievedUsers

--- README.md
+++ README.md
@@ -4,21 +4,5 @@ This README would normally document whatever steps are necessary to get the
 application up and running.
 
 Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+      > kele_client = Kele.new("jane@gmail.com", "abc123")
+       > kele_client.get_me
--- lib/kele.rb
+++ lib/kele.rb
@@ -5,11 +5,18 @@ require "httparty"
 
    def initialize(email, password)
      response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
-     puts response.code
      raise "Invalid email or password" if response.code == 404
      @auth_token = response["auth_token"]
    end
-
+   def get_me
+       response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+       @user_data = JSON.parse(response.body)
+       @user_data.keys.each do |key|
+         self.class.send(:define_method, key.to_sym) do
+           @user_data[key]
+         end
+       end
+     end
  private
 
    def base_api_endpoint(end_point)
