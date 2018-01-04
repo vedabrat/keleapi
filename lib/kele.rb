935e07bb98b92fe5cb05c1fd22abd66345ca49d4
vedabrat <vedabrat>
2018-01-04 18:15:27 -0500

    created CheckPointSubmission method and it's corresponding output

--- lib/kele.rb
+++ lib/kele.rb
@@ -39,6 +39,11 @@ require "httparty"
      puts response
    end
 
+   def create_submissions(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+     response = self.class.post(base_api_endpoint("checkpoint_submissions"), body: { "checkpoint_id": checkpoint_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment }, headers: { "authorization" => @auth_token })
+     puts response
+   end
+
  private
 
    def base_api_endpoint(end_point)
