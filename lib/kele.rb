a15209f37b288ed13dbf60e1db13ca058aab0d07
vedabrat <vedabrat>
2018-01-04 15:12:01 -0500

    refactored

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,30 +8,26 @@ require "httparty"
      raise "Invalid email or password" if response.code == 404
      @auth_token = response["auth_token"]
    end
+
    def get_me
      response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
-     @user_data = JSON.parse(response.body)
-     @user_data.keys.each do |key|
-       self.class.send(:define_method, key.to_sym) do
-         @user_data[key]
-       end
-     end
+     JSON.parse(response.body)
    end
 
    def get_mentor_availability(mentor_id)
      response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
-     @mentor_availability = JSON.parse(response.body)
+     JSON.parse(response.body)
    end
 
-     def get_roadmap(roadmap_id)
-        response = self.class.get(api_url("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
-        @roadmap = JSON.parse(response.body)
-      end
+   def get_roadmap(roadmap_id)
+      response = self.class.get(api_url("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
+      JSON.parse(response.body)
+   end
 
-      def get_checkpoint(checkpoint_id)
-        response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
-        @checkpoint = JSON.parse(response.body)
-      end
+   def get_checkpoint(checkpoint_id)
+     response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+     JSON.parse(response.body)
+   end
 
  private
 
